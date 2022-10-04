624e012b7fee7aade582dadca90b5970da3fae33
agesa3 <collinsagesa2000@gmail.com>
2022-10-04 23:54:43 +0300

    added the navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+    <Navbar />
+    </>
   );
 }
 
