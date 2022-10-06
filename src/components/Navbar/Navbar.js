daf020cebb330a13f73f92cbb6f04998369ee575
agesa3 <collinsagesa2000@gmail.com>
2022-10-06 21:23:50 +0300

    modified the navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
   const handleClose = () => setNav(!nav);
 
   return (
-    <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+    <div className="w-screen h-[80px] z-10 bg-slate-900 text-gray-300 fixed top-0">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
           <h1 className="text-3xl font-bold mr-4 sm:text-4xl">AJIRA PAY.</h1>
@@ -25,28 +25,12 @@ const Navbar = () => {
                 About
               </Link>
             </li>
-            <li>
-              <Link to="support" smooth={true} offset={-50} duration={500}>
-                Support
-              </Link>
-            </li>
-            <li>
-              <Link to="platforms" smooth={true} offset={-100} duration={500}>
-                Platforms
-              </Link>
-            </li>
-            <li>
-              <Link to="pricing" smooth={true} offset={-50} duration={500}>
-                Pricing
-              </Link>
-            </li>
           </ul>
         </div>
         <div className="hidden md:flex pr-4">
-          <button className="border-none bg-transparent text-black mr-4">
-            Sign In
+          <button className="border bg-transparent px-2 py-2 text-white mr-4">
+            Connect Wallet
           </button>
-          <button className="px-8 py-3">Sign Up</button>
         </div>
         <div className="md:hidden mr-4" onClick={handleClick}>
           {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
@@ -54,12 +38,12 @@ const Navbar = () => {
       </div>
 
       <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
-        <li className="border-b-2 border-zinc-300 w-full">
+        <li className="w-full text-black">
           <Link onClick={handleClose} to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className="border-b-2 border-zinc-300 w-full">
+        <li className="w-full text-black">
           <Link
             onClick={handleClose}
             to="about"
@@ -70,45 +54,8 @@ const Navbar = () => {
             About
           </Link>
         </li>
-        <li className="border-b-2 border-zinc-300 w-full">
-          <Link
-            onClick={handleClose}
-            to="support"
-            smooth={true}
-            offset={-50}
-            duration={500}
-          >
-            Support
-          </Link>
-        </li>
-        <li className="border-b-2 border-zinc-300 w-full">
-          <Link
-            onClick={handleClose}
-            to="platforms"
-            smooth={true}
-            offset={-100}
-            duration={500}
-          >
-            Platforms
-          </Link>
-        </li>
-        <li className="border-b-2 border-zinc-300 w-full">
-          <Link
-            onClick={handleClose}
-            to="pricing"
-            smooth={true}
-            offset={-50}
-            duration={500}
-          >
-            Pricing
-          </Link>
-        </li>
-
         <div className="flex flex-col my-4">
-          <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">
-            Sign Inn 
-          </button>
-          <button className="px-8 py-3">Sign Up</button>
+          <button className="px-8 py-3 bg-slate-900">Connect Wallet</button>
         </div>
       </ul>
     </div>
