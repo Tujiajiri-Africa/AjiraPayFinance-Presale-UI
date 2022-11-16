3cd9943fe8474db7089615f2261cd0abfa59f18a
dickensodera <dickens@laevitas.ch>
2022-11-16 04:16:34 +0300

    update: Update timer

--- src/components/Timer/TimeComponent.js
+++ src/components/Timer/TimeComponent.js
@@ -6,7 +6,7 @@ import { PresaleContext }  from '../../context/PresaleContext';
 
 const TimeComponent = () => {
   const { tokenSaleDuration } = useContext(PresaleContext);
-  const THIRTY_DAYS_IN_MS = 35 * 24 * 60 * 60 * 1000;
+  const THIRTY_DAYS_IN_MS = 20 * 24 * 60 * 60 * 1000;
   // let daysToCount;
   // if(tokenSaleDuration !== undefined || tokenSaleDuration >0){
   //   daysToCount = tokenSaleDuration * 1000;
@@ -17,13 +17,14 @@ const TimeComponent = () => {
   const NOW_IN_MS = new Date().getTime() ;
 
   const dateTimeAfterThirtyDays = NOW_IN_MS + tokenSaleDuration;
+  const defaultTimeAfterThirtyDays = NOW_IN_MS + THIRTY_DAYS_IN_MS;
   var delta = Math.abs(tokenSaleDuration - NOW_IN_MS) / 1000;
   return (
     <>
     {tokenSaleDuration  > 0 ?
       <CountdownTimer targetDate={dateTimeAfterThirtyDays} />
       :
-      ''
+      <CountdownTimer targetDate={defaultTimeAfterThirtyDays} />
     }
     </>
   );
